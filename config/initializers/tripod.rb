2bb368e5ae058663e4b6c1fcb3912768a0c956e8
Alex Lynham <acelynham@hotmail.com>
2015-02-12 14:06:07 +0000

    Commented out some Tripod config.

--- config/initializers/tripod.rb
+++ config/initializers/tripod.rb
@@ -1,7 +1,7 @@
 Tripod.configure do |config|
 
   config.cache_store = Tripod::CacheStores::MemcachedCacheStore.new('localhost:11211') || nil
-  config.response_limit_bytes = PublishMyData.response_limit_bytes
+  #config.response_limit_bytes = nil
   config.timeout_seconds = 60
   config.logger = Rails.logger
 
