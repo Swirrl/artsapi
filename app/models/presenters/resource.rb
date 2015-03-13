4795ee8da4b989cb6e069f59cc5f9b77285cf85d
Alex Lynham <acelynham@hotmail.com>
2015-03-13 21:23:03 +0000

    Do not load connections unless they are already present in the fields.

--- app/models/presenters/resource.rb
+++ app/models/presenters/resource.rb
@@ -50,12 +50,12 @@ module Presenters
     # if there are less than 10 connections, they've probably been created by other resources
     # that have written to this one, the list is likely to be much larger
     def connections
-      resource.connections.length < 10 ? generate_and_get_connections : resource.connections.count
+      resource.connections.length < 10 ? generate_and_get_connections : "#{resource.connections.count} Connections"
     end
 
     def generate_and_get_connections
       resource.generate_connections_async
-      resource.get_connections.count
+      "The data is currently loading - please refresh the page in a couple of minutes."
     end
 
     def person?
