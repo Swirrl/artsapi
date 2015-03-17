a36f6d55cb41752e3f754be8e15c35c3254ec6a0
Alex Lynham <acelynham@hotmail.com>
2015-03-17 13:49:20 +0000

    Wording fix.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   def add_connection_advice?(connections, uri)
     if connections.nil?
       html = "<p id='connections-notice'>"
-      html << "<em>There are a small number of known connections for this person.<br>"
+      html << "<em>There are only a small number of known connections for this person.<br>"
       html << "To recalculate, <a href='#' data-uri='#{uri.to_s}' id='recalculate-connections'>click here</a>"
       html << " then refresh the page in a couple of minutes.</em>"
       html << "</p>"
