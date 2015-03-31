c6a77b5a0048eb13efdd16192b03ac01961f7683
Alex Lynham <acelynham@hotmail.com>
2015-03-31 10:59:54 +0100

    Correct typo.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ end
 
 Tripod.configure do |config|
 
-  config.update_endpoint = ENV['FUSEKI_UPDATE_ENDPOINT'].nil? ? "http://#{ARTSAPI_FUSEKI_TCP_ADDR}:3030/artsapi-dev/update" : "#{ENV['FUSEKI_UPDATE_ENDPOINT']}" # check if we're in staging
-  config.query_endpoint = ENV['FUSEKI_QUERY_ENDPOINT'].nil? ? "http://#{ARTSAPI_FUSEKI_TCP_ADDR}:3030/artsapi-dev/sparql" : "#{ENV['FUSEKI_QUERY_ENDPOINT']}" # check if we're in staging
+  config.update_endpoint = ENV['FUSEKI_UPDATE_ENDPOINT'].nil? ? "http://#{ENV['ARTSAPI_FUSEKI_TCP_ADDR']}:3030/artsapi-dev/update" : "#{ENV['FUSEKI_UPDATE_ENDPOINT']}" # check if we're in staging
+  config.query_endpoint = ENV['FUSEKI_QUERY_ENDPOINT'].nil? ? "http://#{ENV['ARTSAPI_FUSEKI_TCP_ADDR']}:3030/artsapi-dev/sparql" : "#{ENV['FUSEKI_QUERY_ENDPOINT']}" # check if we're in staging
 
   # memcached exists in this image
   config.cache_store = Tripod::CacheStores::MemcachedCacheStore.new("http://127.0.0.1:11211")
