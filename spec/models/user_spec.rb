8a109a1e8e40f29f98a3c3c230288dbd85f24469
Alex Lynham <acelynham@hotmail.com>
2015-05-08 16:57:12 +0100

    More tests for .within method.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,13 @@ describe User do
     ds_name_slug: 'artsapi-test'
   ) }
 
+  let(:test_user_two) { User.new(
+    email: 'jeff@example.com',
+    password: 'password',
+    name: 'Jeff Vader',
+    ds_name_slug: 'bar'
+  ) }
+
   describe 'attributes' do
 
     it { expect(test_user).to respond_to(:email) }
@@ -48,7 +55,39 @@ describe User do
   end
 
   describe '#within method, multiple users with different dbs' do
-    pending
+    before do
+      test_user.ds_name_slug = 'foo'
+      test_user.save
+
+      test_user.within {}
+      test_user_two.within {}
+    end
+
+    # teardown - reset to default
+    after do
+      test_user.ds_name_slug = 'artsapi-test'
+      test_user.save
+
+      test_user.within {}
+    end
+
+    it "should modify Tripod query endpoint" do
+      expect(Tripod.query_endpoint).to eq "http://localhost:3030/bar/sparql"
+    end
+
+    it "should modify Tripod update endpoint" do
+      expect(Tripod.update_endpoint).to eq "http://localhost:3030/bar/update"
+    end
+
+    it "should modify Tripod query endpoint based on the user" do
+      test_user.within {}
+      expect(Tripod.query_endpoint).to eq "http://localhost:3030/foo/sparql"
+    end
+
+    it "should modify Tripod update endpoint based on the user" do
+      test_user.within {}
+      expect(Tripod.update_endpoint).to eq "http://localhost:3030/foo/update"
+    end
   end
 
 end
