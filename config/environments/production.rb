bc668cae13d302556d9954c2a35bac9e3b6638f8
Alex Lynham <acelynham@hotmail.com>
2015-06-03 14:22:56 +0100

    HOTFIX: Remove caching layer to ensure database endpoints are correctly set. Need to check memoist calls as well.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # memcached exists in this image
-  config.cache_store = :dalli_store, "http://127.0.0.1:11211", { :namespace => 'artsapi', :expires_in => 1.year, :compress => true }
+  # config.cache_store = :dalli_store, "http://127.0.0.1:11211", { :namespace => 'artsapi', :expires_in => 1.year, :compress => true }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -97,7 +97,7 @@ Tripod.configure do |config|
   config.query_endpoint = ENV['FUSEKI_QUERY_ENDPOINT'].nil? ? "http://#{ENV['ARTSAPI_FUSEKI_PORT_3030_TCP_ADDR']}:3030/artsapi/sparql" : "#{ENV['FUSEKI_QUERY_ENDPOINT']}" # check if we're in staging
 
   # memcached exists in this image
-  config.cache_store = Tripod::CacheStores::MemcachedCacheStore.new("localhost:11211")
+  # config.cache_store = Tripod::CacheStores::MemcachedCacheStore.new("localhost:11211")
   config.timeout_seconds = 240
   config.response_limit_bytes = 40.megabytes
 
